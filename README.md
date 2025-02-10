fcf7df7f77a080eea28774f29b8198e647015393
Fuwn <contact@fuwn.me>
2025-02-10 00:29:18 -0800

    feat: initial release

---
+++ README.md
@@ -0,0 +1,33 @@
+# 🥟 HoloList to JSON
+
+> Dump hololive & HOLOSTARS Birthdays from [HoloList](https://hololist.net) to JSON
+
+## Latest Data
+
+A JSON file containing the *latest* HoloList data is present in the [`latest.json`](https://raw.githubusercontent.com/Fuwn/hololist-to-json/refs/heads/main/latest.json)
+file.
+
+## Sample Data
+
+```json
+[
+  {
+    "month": 1,
+    "day": 12,
+    "name": "Usada Pekora",
+    "pictureURL": "https://hololist.net/wp-content/uploads/2022/02/usada-pekora-portrait-66-300x300.jpg",
+    "profileURL": "https://hololist.net/usada-pekora/"
+  },
+  {
+    "month": 12,
+    "day": 12,
+    "name": "Kobo Kanaeru",
+    "pictureURL": "https://hololist.net/wp-content/uploads/2022/03/kobo-kanaeru-portrait-66-300x300.jpg",
+    "profileURL": "https://hololist.net/kobo-kanaeru/"
+  }
+]
+```
+
+## License
+
+This project is licensed under the terms of the [GNU General Public License v3.0 license](./LICENSE.txt).
---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import bs4
+import json
+
+response = requests.get("https://hololist.net/hololive-birthdays/")
+
+response.raise_for_status()
+
+birthdays = []
+
+for month_div in bs4.BeautifulSoup(response.text, "html.parser").find_all(
+    "div", {"label": "month"}
+):
+    month = int(month_div["id"])
+
+    for row in month_div.find_all("div", class_="row"):
+        for entry in row.find_all("div", class_="col-12"):
+            # Day
+            day_tag = entry.find("h3", class_="fs-6")
+            day = int(day_tag.text.split()[-1]) if day_tag else None
+
+            # Name and profile URL
+            profile_tag = entry.find("a", class_="line-truncate")
+            name = profile_tag.text.strip() if profile_tag else None
+            profile_url = profile_tag["href"] if profile_tag else None
+
+            # Picture URL
+            img_tag = entry.find("img", class_="lazy-image")
+            picture_url = (
+                img_tag["data-src"] if img_tag and "data-src" in img_tag.attrs else None
+            )
+
+            if day and name and profile_url and picture_url:
+                birthdays.append(
+                    {
+                        "month": month,
+                        "day": day,
+                        "name": name,
+                        "pictureURL": picture_url,
+                        "profileURL": profile_url,
+                    }
+                )
+
+with open("hololive_birthdays.json", "w", encoding="utf-8") as json_file:
+    json.dump(birthdays, json_file, ensure_ascii=False, indent=4)
